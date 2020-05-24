d889ad818909d43680a2fb15d98d3d532b89d8b5
Kamal Ezzikouri <kamalezzikouri@gmail.com>
2020-05-23 19:06:37 -0700

    fix name from Trunk to Thunk

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 
-import Trunk from 'redux-thunk';
+import Thunk from 'redux-thunk';
 
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { searchRobots } from './reducers/reducers';
 
-const middleware = [Trunk]
+const middleware = [Thunk]
 
 const store = createStore(
     searchRobots, 
